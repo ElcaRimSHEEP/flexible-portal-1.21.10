ed5c002d27badfbdc5f07827c451e94f0ed55ed0
idleSHEEP <woolgatherer@outlook.com>
2025-11-04 13:30:21 +0800

    Initial commit

---
+++ src/main/java/com/elcarimsheep/flexible_portal/FlexiblePortalDataGenerator.java
@@ -0,0 +1,11 @@
+package com.elcarimsheep.flexible_portal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlexiblePortalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
